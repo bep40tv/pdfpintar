53283c2a77196040f5a7ede7ab751aaf7b37cc35
ahmadrosid <alahmadrosid@gmail.com>
2023-12-07 21:27:32 +0700

    chore: tidy up

--- vite.config.js
+++ vite.config.js
@@ -14,7 +14,7 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            "ziggy-js": path.resolve('vendor/tightenco/ziggy/dist'),
+            "ziggy-js": path.resolve('vendor/tightenco/ziggy'),
         },
     },
 });
